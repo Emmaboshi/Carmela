afab4ba10272cf5ea4c90f300cc17d9d50482a3f
Andrea De Carolis <ciao@decaro.la>
2016-02-01 17:35:09 +0100

    gallery molecule

--- source/js/app/app.js
+++ source/js/app/app.js
@@ -1,5 +1,7 @@
-// $(function(){
-// 	"use strict";
-// 	@import "data-toggleclass.js";
-// });
+$(function(){
+	"use strict";
+	// @import "data-toggleclass.js";
+
+	 $('.j-gallery').slick();
+});
 // @import "js-no-js.js"; // depends on jQuery
--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,7 @@
-// @import "vendor/jquery.min.js";
+
+@import "vendor/jquery-2.2.0.min.js";
+@import "vendor/slick.min.js";
+
 @import "app/app.js";
+
 // @import "vendor/picturefill.js";
