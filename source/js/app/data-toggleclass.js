db83fdc2a3c6f3713d1a86489a687569c9a798f0
Andrea De Carolis <ciao@decaro.la>
2016-02-17 15:29:12 +0100

    header on mobile ok

--- source/js/app/data-toggleclass.js
+++ source/js/app/data-toggleclass.js
@@ -1,23 +1,24 @@
 /*
-	<a href="javascript:;" data-toggleclass-target=".j-something" data-toggleclass="aClass">Toggles .aClass on .j-something</a>
-	<a href="javascript:;" data-toggleclass-target=".j-something" data-toggleclass-exclusive="aClass">Toggles .aClass on .j-something and removes .aClass from .j-something's siblings</a>
-	<a href="javascript:;" data-toggleclass-target=".j-something" data-toggleclass-exclusive-add="aClass">Adds .aClass on .j-something and removes .aClass from .j-something's siblings</a>
 	<a href="javascript:;" data-toggleclass-target=".j-something">Toggles .isActive on .j-something</a>
+	<a href="javascript:;" data-toggleclass-target=".j-something" data-toggleclass="aClass">Toggles .aClass on .j-something</a>
 	<a href="javascript:;" data-toggleclass-target=".j-something" data-toggleclass-add="aClass">Adds .aClass on .j-something</a>
 	<a href="javascript:;" data-toggleclass-target=".j-something" data-toggleclass-remove="aClass">Removes .aClass from .j-something</a>
 	<a href="javascript:;" data-toggleclass-target=".j-something" data-toggleclass-remove="aClass" data-toggleclass-add="bClass">Removes .aClass from .j-something and adds .bClass to it</a>
+	<a href="javascript:;" data-toggleclass-target=".j-something" data-toggleclass-exclusive="aClass">Toggles .aClass on .j-something and removes .aClass from .j-something's siblings</a>
+	<a href="javascript:;" data-toggleclass-target=".j-something" data-toggleclass-exclusive-add="aClass">Adds .aClass on .j-something and removes .aClass from .j-something's siblings</a>
 */
 
 (function() {
 
 	var clickToggle = function(e){
 		e.preventDefault();
+		console.log("ioio");
 
 		var $this=$(this);
 		if(!$this.hasClass("js-toggleclass-disabled")){
 			var
 				$target=$($this.data("toggleclass-target")),
-				exclusive=$this.data("toggleclass-exclusive");
+				exclusive=$this.data("toggleclass-exclusive"),
 				exclusive_add=$this.data("toggleclass-exclusive-add");
 			//var
 			if(exclusive) {
